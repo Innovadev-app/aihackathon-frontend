e7cd364aa4fedf5646ba53489add1e0a771dc7a7
George Rolston <grolston@outlook.com>
2023-10-06 20:43:31 -0600

    api add

--- timothyai/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ timothyai/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "timothyai": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "timothyai505ac746": {
       "AppClientID": "string",
