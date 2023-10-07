eacec0e44800f9e3b2bb8d1f511498ba20f1df3d
George Rolston <grolston@outlook.com>
2023-10-06 20:37:54 -0600

    rn init

---
+++ timothyai/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "timothyai505ac746": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
