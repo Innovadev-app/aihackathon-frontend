99a03615fdd58b8ccce086a769bd846c5a15bbcc
Andrew Fetting <afetting@outlook.com>
2023-10-07 22:35:12 -0500

    Updated profile.tsx

--- timothyai/app/profile.tsx
+++ timothyai/app/profile.tsx
@@ -1,10 +1,52 @@
-import { Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 export default function Profile() {
   return (
-    <View className="mx-6 mt-8">
-      <Text className='text-3xl font-bold text-white mb-6'>My Profile</Text>
-      <Text className='text-xl font-bold text-white mt-1 mb-24'>My Church</Text>
-    </View>
+      <View>
+        <View className="mx-6 mt-8">
+          <Text className='text-3xl font-bold text-white mb-6'>My Profile</Text>
+          <Text className='text-xl font-bold text-white mt-1 mb-8'>My Church</Text>
+        </View>
+            <View className="mx-24 mt-0">
+                <Button title={'Connect Church'}/>
+            </View>
+        <View className="mx-6 mt-8">
+            <BouncyCheckbox text={'I would like to be an accountability\n' +
+                'partner'} onPress={(isChecked: boolean) => {}} />
+        </View>
+          <View className="mx-6 mt-8">
+              <BouncyCheckbox text={"I would like to be a prayer partner"} onPress={(isChecked: boolean) => {}} />
+          </View>
+          <View className={"mx-2 mt-2"}>
+              <TextInput style={styles.input} placeholder="First Name" placeholderTextColor={"#ffffff"}></TextInput>
+          </View>
+          <View className={"mx-2 mt-2"}>
+              <TextInput style={styles.input} placeholder="Last Name" placeholderTextColor={"#ffffff"}></TextInput>
+          </View>
+          <View className={"mx-2 mt-2"}>
+              <TextInput style={styles.input} placeholder="Email Address" placeholderTextColor={"#ffffff"}></TextInput>
+          </View>
+          <View className={"mx-2 mt-2"}>
+              <Text className='text-xl font-bold text-white'>{"Nudges"}</Text>
+          </View>
+          <View className={"mx-2 mt-0"}>
+              <TextInput style={styles.input} placeholder="Daily" placeholderTextColor={"#ffffff"}></TextInput>
+          </View>
+      </View>
   );
+
 }
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: "#1E1E1E",
+        fontSize: 17
+    },
+});
+
+
