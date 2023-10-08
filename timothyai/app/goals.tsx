3f094867049e51771f0652bff796f50223adae62
Kurt <kurt.hopfer@effectual.com>
2023-10-07 17:56:45 -0700

    Initial goals submit

--- timothyai/app/goals.tsx
+++ timothyai/app/goals.tsx
@@ -1,10 +1,81 @@
-import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import {View, SafeAreaView, StyleSheet, Text, Platform, UIManager} from 'react-native';
+import {AccordionList} from 'react-native-accordion-list-view';
+
+const styles = StyleSheet.create({
+  container: {
+      paddingVertical: '2%',
+      paddingHorizontal: '3%',
+      height: '100%',
+      backgroundColor: '#e7e7e7',
+  },
+});
+
 
 export default function Goals() {
+  const handleServerResponse = (responseData: any) => {
+    console.log(JSON.stringify(responseData));
+  };
+
+ fetch("https://1lqp8lahll.execute-api.us-west-2.amazonaws.com/prod/timothy-chat", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    }
+  })
+      .then((response) => response.json())
+      .then(handleServerResponse);
+
+
+
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+        if (UIManager.setLayoutAnimationEnabledExperimental) {
+            UIManager.setLayoutAnimationEnabledExperimental(true);
+        }
+    }
+}, []);
+
+  const data = [{
+      title: "You are a Christian",
+      body: "John 3:16 \"For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.\n Romans 10:9 \"If you declare with your mouth, 'Jesus is Lord,' and believe in your heart that God raised him from the dead, you will be saved.\""
+    },
+    {
+      title: "You belong to a local church",
+      body: "Attend more"
+    },
+    {
+      title: "You are seeking God's forgiveness",
+      body: "You already have it"
+    },
+    {
+      title: "God feels very distant at this time in your life",
+      body: "Close the Gap"
+    },
+    {
+      title: "You have a good knowledge of Bible background and history",
+      body: "There's always room to know more"
+    }];
+
+
+
+  
   return (
     <View className="mx-6 mt-8">
       <Text className='text-3xl font-bold text-white'>Goals</Text>
-      <Text className='text-xs text-white mt-1'>We’ve curated relevant, world class resources bases on your conversation with Timothy</Text>
+      <Text className='text-xs text-white mt-1'>We’ve curated relevant, world class resources based on your conversation with Timothy</Text>
+            <View style={{paddingTop:10, backgroundColor:"#212121"}} >
+                <AccordionList
+                    data={data}
+                    customTitle={item => <Text>{item.title}</Text>}
+                    customBody={item => <Text>{item.body}</Text>}
+                    animationDuration={400}
+                    expandMultiple={true}
+
+                />
+            </View>
     </View>
   );
 }
