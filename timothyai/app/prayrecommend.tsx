d38ddf51e1eb3565f1ec968d2d8e97eb09f15526
Andrew Fetting <afetting@outlook.com>
2023-10-07 23:06:41 -0500

    Updated profile.tsx

--- timothyai/app/prayrecommend.tsx
+++ timothyai/app/prayrecommend.tsx
@@ -3,8 +3,29 @@ import { Text, View } from 'react-native';
 export default function Recommend() {
 
   return (
+      <View>
     <View className="mx-6 mt-8">
       <Text className='text-3xl font-bold text-white mb-6'>Recommend</Text>
     </View>
+          <View className="mx-6 mt-16">
+              <Text className='font-medium text-white mb-6'>Dear Lord,</Text>
+              <Text className='font-medium text-white mb-6'>
+                  Thank you for giving me the gifts
+                  and talents that I have. I want to use
+                  them to serve you and help others.
+                  Please give me the strength and
+                  wisdom to use them to the best of
+                  my ability.</Text>
+              <Text className='font-medium text-white mb-6'>
+                  I also ask that you help me to use
+                  my time and resources wisely. I
+                  want to live a life that is pleasing to
+                  you, and I want to do my part to
+                  spread your word and help others.</Text>
+              <Text className='font-medium text-white mb-6'>
+                  Thank you, Lord, for your blessings
+                  and guidance. Amen.</Text>
+          </View>
+      </View>
   );
 }
--- timothyai/app/prayrecord.tsx
+++ timothyai/app/prayrecord.tsx
@@ -1,10 +1,29 @@
 import { Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function PrayerRecord() {
 
   return (
-    <View className="mx-6 mt-8">
-      <Text className='text-3xl font-bold text-white mb-6'>Record</Text>
+    <View>
+        <View className="mx-6 mt-8">
+            <Text className='text-3xl font-bold text-white mb-6'>Record</Text>
+        </View>
+        <View className="mx-6 mt-8 items-center justify-center">
+            <FontAwesome name="microphone" size={80} color="white" />
+        </View>
+        <View className="mx-6 mt-16">
+            <Text className='font-medium text-white mb-6'>Dear God,</Text>
+            <Text className='font-medium text-white mb-6'>
+                We pray for Bob, who is struggling
+                with a knee injury.
+                We ask that you bring healing to his
+                knee, and give him the strength and
+                courage to face this challenge.
+                We also ask for your guidance, and
+                for the ability to trust in your care.
+                Amen.</Text>
+        </View>
     </View>
+
   );
 }
