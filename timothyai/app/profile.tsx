c95c5eea804688db6bcbfe5747be8450e6408883
Bill Stock <bill@wrsdevelopment.com>
2023-10-08 00:33:52 -0600

    Update profile for scrollview

--- timothyai/app/profile.tsx
+++ timothyai/app/profile.tsx
@@ -1,41 +1,41 @@
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Profile() {
-  return (
-      <View>
-        <View className="mx-6 mt-8">
-          <Text className='text-3xl font-bold text-white mb-6'>My Profile</Text>
-          <Text className='text-xl font-bold text-white mt-1 mb-8'>My Church</Text>
-        </View>
-            <View className="mx-24 mt-0">
-                <Button title={'Connect Church'}/>
+    return (
+        <ScrollView>
+            <View className="mx-6 mt-8">
+                <Text className='text-3xl font-bold text-white mb-6'>My Profile</Text>
+                <Text className='text-xl font-bold text-white mt-1 mb-8'>My Church</Text>
             </View>
-        <View className="mx-6 mt-8">
-            <BouncyCheckbox text={'I would like to be an accountability\n' +
-                'partner'} onPress={(isChecked: boolean) => {}} />
-        </View>
-          <View className="mx-6 mt-8">
-              <BouncyCheckbox text={"I would like to be a prayer partner"} onPress={(isChecked: boolean) => {}} />
-          </View>
-          <View className={"mx-2 mt-2"}>
-              <TextInput style={styles.input} placeholder="First Name" placeholderTextColor={"#ffffff"}></TextInput>
-          </View>
-          <View className={"mx-2 mt-2"}>
-              <TextInput style={styles.input} placeholder="Last Name" placeholderTextColor={"#ffffff"}></TextInput>
-          </View>
-          <View className={"mx-2 mt-2"}>
-              <TextInput style={styles.input} placeholder="Email Address" placeholderTextColor={"#ffffff"}></TextInput>
-          </View>
-          <View className={"mx-2 mt-2"}>
-              <Text className='text-xl font-bold text-white'>{"Nudges"}</Text>
-          </View>
-          <View className={"mx-2 mt-0"}>
-              <TextInput style={styles.input} placeholder="Daily" placeholderTextColor={"#ffffff"}></TextInput>
-          </View>
-      </View>
-  );
+            <View className="mx-24 mt-0">
+                <Button title={'Connect Church'} />
+            </View>
+            <View className="mx-6 mt-8">
+                <BouncyCheckbox text={'I would like to be an accountability\n' +
+                    'partner'} onPress={(isChecked: boolean) => { }} />
+            </View>
+            <View className="mx-6 mt-8">
+                <BouncyCheckbox text={"I would like to be a prayer partner"} onPress={(isChecked: boolean) => { }} />
+            </View>
+            <View className={"mx-2 mt-2"}>
+                <TextInput style={styles.input} placeholder="First Name" placeholderTextColor={"#ffffff"}></TextInput>
+            </View>
+            <View className={"mx-2 mt-2"}>
+                <TextInput style={styles.input} placeholder="Last Name" placeholderTextColor={"#ffffff"}></TextInput>
+            </View>
+            <View className={"mx-2 mt-2"}>
+                <TextInput style={styles.input} placeholder="Email Address" placeholderTextColor={"#ffffff"}></TextInput>
+            </View>
+            <View className={"mx-2 mt-2"}>
+                <Text className='text-xl font-bold text-white'>{"Nudges"}</Text>
+            </View>
+            <View className={"mx-2 mt-0"}>
+                <TextInput style={styles.input} placeholder="Daily" placeholderTextColor={"#ffffff"}></TextInput>
+            </View>
+        </ScrollView>
+    );
 
 }
 
